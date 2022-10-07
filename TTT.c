e65fd4981691be177689bdd44dfe4d822fc98b91
Aliuddin Khaja <aokhaja@cpp.edu>
2022-10-07 15:02:41 -0700

    Initial push with gameplan

---
+++ TTT.c
@@ -0,0 +1,27 @@
+/* write a function here that determines whether the game will be between a 
+computer vs player or player vs player will be a simple if statement that 
+will lead to seperate sequences for each option. */ 
+
+
+/* write a function for displaying the board as well as assigning numbers
+to the open parts of the table so that inputing O's and X's are easier */
+
+
+/* write a function to add in the X's and O's into the respective slots that
+the player and/or computer picks*/
+
+/* write a function that checks the win conditions if a row, column, or diagnal is 
+filled with the same caracters */
+
+/* write a function that checks if all of the slots are filled up. if all the slots are 
+filled up and none of the win conditions are met print out "draw" */
+
+
+/*write a algorithym for the computer to check if a spot is empty and input a move
+if that spot is empty otherwise find an open spot and input. */
+
+/*write a function to switch the turn from P to P and from C to P*/
+
+
+/*end program*/
+
